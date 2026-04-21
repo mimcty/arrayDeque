efb46ebae353171ae596965a3090de98ae978dbb
Michelle Ma <michellema.5183@gmail.com>
2026-04-21 01:20:26 -0400

    Write tests and working code for deque implemented with backing array without resizing function.

--- src/deque/ArrayDeque61B.java
+++ src/deque/ArrayDeque61B.java
@@ -1,51 +1,90 @@
 package deque;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayDeque61B<T> implements Deque61B<T> {
+    T[] items;
+    int size;
+    int nextFirst;
+    int nextLast;
+
+    @SuppressWarnings("unchecked")
+    public ArrayDeque61B() {
+        this.items = (T[]) new Object[8];
+        this.size = 0;
+        this.nextFirst = 7;
+        this.nextLast = 0;
+    }
 
     @Override
     public void addFirst(T x) {
-
+        items[nextFirst] = x;
+        size++;
+        nextFirst--;
     }
 
     @Override
     public void addLast(T x) {
-
+        items[nextLast] = x;
+        size++;
+        nextLast++;
     }
 
     @Override
     public List<T> toList() {
-        return List.of();
+        List<T> retList = new ArrayList<>();
+        int i = nextFirst + 1;
+        for (int j = 0; j < size; j++) {
+            retList.add(items[Math.floorMod(i, items.length)]);
+            i++;
+        }
+        return retList;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public T removeFirst() {
-        return null;
+        int firstIdx = Math.floorMod(nextFirst + 1, items.length);
+        T first = items[firstIdx];
+        items[firstIdx] = null;
+        size--;
+        nextFirst = firstIdx;
+        return first;
     }
 
     @Override
     public T removeLast() {
-        return null;
+        int lastIdx = Math.floorMod(nextLast - 1, items.length);
+        T last = items[lastIdx];
+        items[lastIdx] = null;
+        size--;
+        nextLast = lastIdx;
+        return last;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        // return null if index out of bounds
+        if (index < 0 || index >= size) {
+            return null;
+        } else {
+            int first = nextFirst + 1;
+            return items[Math.floorMod(first + index, items.length)];
+        }
     }
 
     @Override
     public T getRecursive(int index) {
-        return null;
+        throw new UnsupportedOperationException("No need to implement getRecursive for proj 1b");
     }
 }
--- tests/ArrayDeque61BTest.java
+++ tests/ArrayDeque61BTest.java
@@ -1,4 +1,5 @@
 import deque.ArrayDeque61B;
+import deque.Deque61B;
 import jh61b.utils.Reflection;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,15 +11,207 @@ import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDeque61BTest {
+    @Test
+    /* Uses addFirst() to add one element to an empty list. **/
+    public void addFirstToEmptyList() {
+        Deque61B<String> deque = new ArrayDeque61B<>();
+        deque.addFirst("new");
+        assertThat(deque.toList()).containsExactly("new");
+    }
 
-//     @Test
-//     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
-//     void noNonTrivialFields() {
-//         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
-//                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
-//                 .toList();
-//
-//         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
-//     }
+    @Test
+    /* Uses addFirst() to add the same amount of elements as the backing array's size. **/
+    public void addFirstMaxArraySize() {
+        Deque61B<Integer> deque = new ArrayDeque61B<>();
+        for (int i = 0; i < 8; i++) {
+            deque.addFirst(i);
+        }
+        assertThat(deque.toList()).containsExactly(7, 6, 5, 4, 3, 2, 1, 0).inOrder();
+    }
+
+    @Test
+    /* Uses addFirst to add 9 elements to an empty list. Tests resizing; backing array is initialized with size 8. **/
+    public void addFirstTestResize() {
+        Deque61B<Integer> deque = new ArrayDeque61B<>();
+        for (int i = 0; i < 9; i++) {
+            deque.addFirst(i);
+        }
+        assertThat(deque.toList()).containsExactly(0, 1, 2, 3, 4, 5, 6, 7, 8).inOrder();
+    }
+
+    @Test
+    /* Uses addLast() to add one element to an empty list. **/
+    public void addLastToEmptyList() {
+        Deque61B<String> deque = new ArrayDeque61B<>();
+        deque.addLast("new");
+
+        assertThat(deque.toList()).containsExactly("new");
+    }
+
+    @Test
+    /* Uses addLast() to add the same amount of elements as the backing array's size. **/
+    public void addLastMaxArraySize() {
+        Deque61B<Integer> deque = new ArrayDeque61B<>();
+        for (int i = 0; i < 8; i++) {
+            deque.addLast(i);
+        }
+        assertThat(deque.toList()).containsExactly(0, 1, 2, 3, 4, 5, 6, 7).inOrder();
+    }
+
+    @Test
+    /* Uses addLast to add 9 elements to an empty list. Tests resizing; backing array is initialized with size 8. **/
+    public void addLastTestResize() {
+        Deque61B<Integer> deque = new ArrayDeque61B<>();
+        for (int i = 0; i < 9; i++) {
+            deque.addLast(i);
+        }
+        assertThat(deque.toList()).containsExactly(0, 1, 2, 3, 4, 5, 6, 7, 8).inOrder();
+    }
+
+    @Test
+    /* Use addFirst() and addLast() to add elements. **/
+    public void addFirstAndLast() {
+        Deque61B<String> deque = new ArrayDeque61B<>();
+        deque.addFirst("second");
+        deque.addLast("third");
+        deque.addFirst("first");
+        deque.addLast("last");
+        assertThat(deque.toList()).containsExactly("first", "second", "third", "last").inOrder();
+    }
+
+    @Test
+    /* Use addFirst() and addLast() to add more elements than the backing array's initial size. **/
+    public void addFirstAndLastMoreElements() {
+        Deque61B<Integer> deque = new ArrayDeque61B<>();
+        for (int i = 3; i >= 0; i--) {
+            deque.addFirst(i);
+        }
+        for (int i = 4; i < 9; i++) {
+            deque.addLast(i);
+        }
+        assertThat(deque.toList()).containsExactly(0, 1, 2, 3, 4, 5, 6, 7, 8).inOrder();
+    }
+
+    @Test
+    /* Use get() to get a valid index from deque. **/
+    public void getValidIndex() {
+        Deque61B<String> deque = new ArrayDeque61B<>();
+        deque.addFirst("hello");
+        deque.addLast("world");
+        assertThat(deque.get(0)).isEqualTo("hello");
+        assertThat(deque.get(1)).isEqualTo("world");
+    }
+
+    @Test
+    /* Use get() to get an index from an empty array. **/
+    public void getIndexFromEmptyArray() {
+        Deque61B<String> deque = new ArrayDeque61B<>();
+        assertThat(deque.get(0)).isEqualTo(null);
+    }
+
+    @Test
+    /* Use get() to get a very large, out-of-bounds index from an array. **/
+    public void getVeryLargeIndex() {
+        Deque61B<String> deque = new ArrayDeque61B<>();
+        deque.addFirst("hello");
+        assertThat(deque.get(10000)).isEqualTo(null);
+    }
+
+    @Test
+    /* Use get() to get a negative, out-of-bounds index from an array. **/
+    public void getNegativeIndex() {
+        Deque61B<String> deque = new ArrayDeque61B<>();
+        deque.addFirst("hello");
+        assertThat(deque.get(-100)).isEqualTo(null);
+    }
+
+    @Test
+    /* Tests isEmpty() on an empty deque. **/
+    public void testIsEmptyNoElements(){
+        Deque61B<Integer> deque = new ArrayDeque61B<>();
+        assertThat(deque.isEmpty()).isTrue();
+    }
+
+    @Test
+    /* Tests size() on an empty deque. **/
+    public void testIsEmptyHasElements(){
+        Deque61B<Integer> deque = new ArrayDeque61B<>();
+        deque.addFirst(10);
+        assertThat(deque.isEmpty()).isFalse();
+        deque.removeFirst();
+        assertThat(deque.isEmpty()).isTrue();
+    }
+
+    @Test
+    /* Tests size() on an empty deque. **/
+    public void testSizeEmptyDeque(){
+        Deque61B<Integer> deque = new ArrayDeque61B<>();
+        assertThat(deque.size()).isEqualTo(0);
+    }
+
+    @Test
+    /* Tests size() on a non-empty deque. **/
+    public void testSizeNonEmptyDeque(){
+        Deque61B<Integer> deque = new ArrayDeque61B<>();
+        deque.addFirst(10);
+        assertThat(deque.size()).isEqualTo(1);
+        deque.addFirst(9);
+        deque.addFirst(8);
+        assertThat(deque.size()).isEqualTo(3);
+    }
+
+    @Test
+    /* Tests removeFirst() on a deque that has two element. Checks that it contains 0 elements after.**/
+    public void removeFirstTwoElements() {
+        Deque61B<Integer> deque = new ArrayDeque61B<>();
+        deque.addFirst(7);
+        deque.addFirst(6);
+        assertThat(deque.size()).isEqualTo(2);
+        deque.removeFirst();
+        assertThat(deque.toList()).containsExactly(7);
+        assertThat(deque.size()).isEqualTo(1);
+        deque.removeFirst();
+        assertThat(deque.size()).isEqualTo(0);
+    }
+
+    @Test
+    /* Tests removeFirst() on a filled deque. **/
+    public void removeFirstFullDeque() {
+        Deque61B<Integer> deque = new ArrayDeque61B<>();
+        for (int i = 0; i < 8; i++) {
+            deque.addFirst(i);
+        }
+        assertThat(deque.size()).isEqualTo(8);
+        deque.removeFirst();
+        assertThat(deque.toList()).containsExactly(6, 5, 4, 3, 2, 1, 0).inOrder();
+        assertThat(deque.size()).isEqualTo(7);
+    }
+
+    @Test
+    /* Tests removeLast() on a deque that has two element. Checks that it contains 0 elements after.**/
+    public void removeLastTwoElements() {
+        Deque61B<Integer> deque = new ArrayDeque61B<>();
+        deque.addFirst(3);
+        deque.addFirst(4);
+        assertThat(deque.size()).isEqualTo(2);
+        deque.removeLast();
+        assertThat(deque.toList()).containsExactly(4);
+        assertThat(deque.size()).isEqualTo(1);
+        deque.removeLast();
+        assertThat(deque.isEmpty()).isTrue();
+    }
+
+    @Test
+    /* Tests removeLast() on a filled deque. **/
+    public void removeLastFullDeque() {
+        Deque61B<Integer> deque = new ArrayDeque61B<>();
+        for (int i = 0; i < 8; i++) {
+            deque.addFirst(i);
+        }
+        assertThat(deque.size()).isEqualTo(8);
+        deque.removeLast();
+        assertThat(deque.toList()).containsExactly(7, 6, 5, 4, 3, 2, 1).inOrder();
+        assertThat(deque.size()).isEqualTo(7);
+    }
 
 }
